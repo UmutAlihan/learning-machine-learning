dcca9eb546a7597f7b8d671f7a77e8750280c40c
UmutAlihan <alihandikel@gmail.com>
2019-11-05 00:12:20 +0300

    write generation function

--- tf-rnn-text-generation/train.py
+++ tf-rnn-text-generation/train.py
@@ -175,3 +175,60 @@ EPOCHS=10
 history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 
 
+##Generate text
+#Restore the latest checkpoint
+#keep this prediction step simple, use a batch size of 1.
+#Because of the way the RNN state is passed from timestep to timestep, the model only accepts a fixed batch size once built
+tf.train.latest_checkpoint(checkpoint_dir)
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+
+##The prediction loop
+#The following code block generates the text:
+#It Starts by choosing a start string, initializing the RNN state and setting the number of characters to generate.
+#Get the prediction distribution of the next character using the start string and the RNN state.
+#Then, use a categorical distribution to calculate the index of the predicted character. Use this predicted character as our next input to the model.
+#The RNN state returned by the model is fed back into the model so that it now has more context, instead than only one word. After predicting the next word, the modified RNN states are again fed back into the model, which is how it learns as it gets more context from the previously predicted words.
+def generate_text(model, start_string):
+  # Evaluation step (generating text using the learned model)
+
+  # Number of characters to generate
+  num_generate = 1000
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+
+  # Low temperatures results in more predictable text.
+  # Higher temperatures results in more surprising text.
+  # Experiment to find the best setting.
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a categorical distribution to predict the word returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+      # We pass the predicted word as the next input to the model
+      # along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+
+print(generate_text(model, start_string=u"ROMEO: "))
+
