b78e62462420853e06a0bcefc6bc38ac58aaa951
UmutAlihan <alihandikel@gmail.com>
2020-01-18 15:38:38 +0300

    most accurate lstm model ever with epoch:15

--- tf-rnn-text-generation/dervisesor/train.py
+++ tf-rnn-text-generation/dervisesor/train.py
@@ -114,7 +114,7 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
   model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim,
                               batch_input_shape=[batch_size, None]),
-    tf.keras.layers.GRU(rnn_units,
+    tf.keras.layers.LSTM(rnn_units,
                         return_sequences=True,
                         stateful=True,
                         recurrent_initializer='glorot_uniform'),
@@ -174,7 +174,7 @@ checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
     save_weights_only=True)
 
 ##Execute the training
-EPOCHS=30
+EPOCHS=15
 history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 
 
